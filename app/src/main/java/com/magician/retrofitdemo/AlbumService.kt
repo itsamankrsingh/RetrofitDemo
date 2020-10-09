436dbc0f7bc6026e342f91ab7d32ee50417ff171
itsamankrsingh <itsamankrsingh@gmail.com>
2020-10-09 19:40:52 +0530

    added post request

--- app/src/main/java/com/magician/retrofitdemo/AlbumService.kt
+++ app/src/main/java/com/magician/retrofitdemo/AlbumService.kt
@@ -1,10 +1,19 @@
 package com.magician.retrofitdemo
 
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface AlbumService {
 
     @GET("/albums")
-    suspend fun getAlbum():Response<Album>
+    suspend fun getAlbums(): Response<Album>
+
+    @GET("/albums")
+    suspend fun getSortedAlbum(@Query("userId") userId: Int): Response<Album>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") albumId: Int): Response<AlbumItem>
+
+    @POST("/albums")
+    suspend fun uploadAlbum(@Body album: AlbumItem): Response<AlbumItem>
 }
--- app/src/main/java/com/magician/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/magician/retrofitdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.magician.retrofitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -10,15 +11,56 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retService = RetrofitInstance.getRetrofitInstance()
+        retService = RetrofitInstance.getRetrofitInstance()
             .create(AlbumService::class.java)
 
+        //getRequestWithQueryParameter()
+        //getRequestWithPathParameter()
+        uploadItem()
+
+    }
+
+    private fun uploadItem() {
+        val album = AlbumItem(-1, "Tis is my first post requrst", 2069)
+        val postResponse: LiveData<Response<AlbumItem>> = liveData {
+            val response = retService.uploadAlbum(album)
+            emit(response)
+        }
+
+        postResponse.observe(this, Observer {
+            val receivedAlbumItem=it.body()
+            val result=" "+"Album Title: ${receivedAlbumItem!!.title}"+"\n"+
+                    " "+"Album Id: ${receivedAlbumItem!!.id}"+"\n"+
+                    " "+"User Id: ${receivedAlbumItem!!.userId}"+"\n\n\n"
+
+            text_view.append(result)
+        })
+    }
+
+    private fun getRequestWithPathParameter() {
+        //path parameter
+
+        val pathRespomse: LiveData<Response<AlbumItem>> = liveData {
+            val response = retService.getAlbum(3)
+            emit(response)
+        }
+
+        pathRespomse.observe(this, Observer {
+            val title = it.body()!!.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun getRequestWithQueryParameter() {
         val responseLiveData: LiveData<Response<Album>> = liveData {
-            val response = retService.getAlbum()
+            val response = retService.getSortedAlbum(3)
             emit(response)
         }
 
@@ -28,9 +70,9 @@ class MainActivity : AppCompatActivity() {
                 while (albumList.hasNext()) {
                     val albumItem = albumList.next()
                     Log.i("MYTAG", albumItem.title)
-                    val result=" "+"Album Title: ${albumItem.title}"+"\n"+
-                            " "+"Album id: ${albumItem.id}"+"\n"+
-                            " "+"User id: ${albumItem.userId}"+"\n\n\n"
+                    val result = " " + "Album Title: ${albumItem.title}" + "\n" +
+                            " " + "Album id: ${albumItem.id}" + "\n" +
+                            " " + "User id: ${albumItem.userId}" + "\n\n\n"
 
                     text_view.append(result)
                 }
